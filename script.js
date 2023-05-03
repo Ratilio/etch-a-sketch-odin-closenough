6945d9d77d6675075697be8810ed6f370d219a8f
Ratilio <93900286+Ratilio@users.noreply.github.com>
2023-05-03 12:16:13 -0600

    Added comments after understanding the process of the grid.

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-let gridSize = 16;
+let gridSize = 5;
 
 const grid = document.getElementById('grid');
 
 gridCreation(gridSize, gridSize);
 
 function gridCreation(rows, columns){
-  grid.style.setProperty('--grid-size', rows);
-  grid.style.setProperty('--grid-size', columns);
+  //setting the property names and values to use in css
+  grid.style.setProperty('--grid-row', rows);
+  grid.style.setProperty('--grid-col', columns);
+  
   for (let index = 0; index < (rows * columns); index++) {
     let cell = document.createElement('div');
     cell.innerText = (index + 1);
     grid.appendChild(cell).classList.toggle('grid-element');
     
-  }
-
-
-  /*
-  for (let i = 0; i < gridSize; i++) {
-    const gridElement = document.createElement('div');
-    gridElement.classList.toggle('grid-element');
-    grid.appendChild(gridElement);
-    
-  }*/
-  
+  }  
 }
